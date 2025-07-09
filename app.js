59b074b95e44b8fe7f9bdd1c73c202c6fa73200f
Jeyong <dlwpdyd201@naver.com>
2025-07-09 23:35:09 +0900

    [REFACTOR] 프토폴리오 내용 추가

--- app.js
+++ app.js
@@ -1,30 +1,34 @@
 const blogs = document.querySelectorAll('.blog-card');
 const projects = document.querySelectorAll('.project-card');
 const icons = document.querySelectorAll('.icon');
-// 각 blog 요소에 클릭 이벤트 등록
-blogs.forEach(div => {
-    div.addEventListener('click', () => {
-        const url = div.getAttribute('data-href');
-        if (url) {
-            window.location.href = url;
-        }
-    });
-});
 
-projects.forEach(div => {
-    div.addEventListener('click', () => {
-        const url = div.getAttribute('data-href');
-        if (url) {
-            window.location.href = url;
-        }
+function addClickListeners(selector) {
+  document.querySelectorAll(selector).forEach(element => {
+    element.addEventListener('click', () => {
+      const url = element.getAttribute('data-href');
+      if (url) {
+        window.location.href = url;
+      }
     });
-});
+  });
+}
 
-icons.forEach(div => {
-    div.addEventListener('click', () => {
-        const url = div.getAttribute('data-href');
-        if (url) {
-            window.location.href = url;
-        }
-    });
-});
+addClickListeners('.blog-card');
+// addClickListeners('.project-card');
+addClickListeners('.icon');
+
+// function toggleCardContent(toggleBtn) {
+//   const card = toggleBtn.closest('.project-card');
+//   const extraContent = card.querySelector('.card-extra-content');
+//
+//   if (extraContent.style.display === 'none') {
+//     extraContent.style.display = 'block';
+//   } else {
+//     extraContent.style.display = 'none';
+//   }
+// }
+
+function toggleCardContent(toggleBtn) {
+  const card = toggleBtn.closest('.project-card');
+  card.classList.toggle('flipped');
+}
